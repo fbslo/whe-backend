9a371091995df928e1d86a2a80eb85d39d267578
fbslo <admin@fbslo.net>
2022-03-01 22:05:37 +0100

    refund reverted txs

--- src/libs/ethereum/sendEthereumTokens.js
+++ src/libs/ethereum/sendEthereumTokens.js
@@ -84,6 +84,10 @@ async function start(depositAmount, address, sender, logger, depositTransaction)
       console.log(`Error while sending ERC-20 token (out of gas), refunded: ${e}, details: ${JSON.stringify(details)}`)
       logger.log('error', `Error while sending ERC-20 token (out of gas), refunded: ${e}, details: ${JSON.stringify(details)}`)
       refundFailedTransaction(depositAmount, sender, 'Internal server error while processing your request, details: Not enough ETH for gas cost')
+    } else  if ((e).toString().includes("Transaction has been reverted by the EVM:")){
+      console.log(`Error while sending ERC-20 token (EVM reverted), refunded: ${e}, details: ${JSON.stringify(details)}`)
+      logger.log('error', `Error while sending ERC-20 token (EVM reverted), refunded: ${e}, details: ${JSON.stringify(details)}`)
+      refundFailedTransaction(depositAmount, sender, 'Internal server error while processing your request, details: Transaction has been reverted by the EVM')
     } else {
       console.log(`Error NOT refunded: ${e}, details: ${JSON.stringify(details)}`)
       logger.log('error', `Error NOT refunded: ${e}, details: ${JSON.stringify(details)}`)
