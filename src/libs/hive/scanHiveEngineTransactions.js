ded354772f78116309194644ce5335892ba20796
fbslo <admin@fbslo.net>
2022-04-29 17:14:00 +0200

    prevent loops when sending from HIVE_ACCOUNT to HIVE_ACCOUNT

--- src/libs/hive/scanHiveEngineTransactions.js
+++ src/libs/hive/scanHiveEngineTransactions.js
@@ -16,7 +16,7 @@ function start(callback){
         let data = op[1]
 
         if (operation === "transfer" ){
-          if (data.to == process.env.HIVE_ACCOUNT && data.amount.includes("HIVE")){
+          if (data.to == process.env.HIVE_ACCOUNT && data.from != process.env.HIVE_ACCOUNT && data.amount.includes("HIVE")){
             if (!alreadyProcessed.includes(transaction_id)){
               alreadyProcessed.push(transaction_id)
               let tx = {
