18dd247ae278d47dbeca9ec4c85f26d2f59e1abe
fbslo <admin@fbslo.net>
2025-09-28 20:37:24 +0200

    limit block range

--- src/libs/ethereum/scanEthereumTransactions.js
+++ src/libs/ethereum/scanEthereumTransactions.js
@@ -41,6 +41,14 @@ async function getERC20TransactionsByEvent(tokenContractAddress) {
     // let lastProcessedBlock = await getLastProcesedBlock()
     let fromBlock = currentBlockNumber - 3400;
     let toBlock = currentBlockNumber - 12 //wait 12 confirmations
+
+    if (toBlock - fromBlock > 1000){
+      toBlock = fromBlock + 1000
+      if (toBlock > currentBlockNumber - 12){
+        toBlock = currentBlockNumber - 12
+      }
+    }
+
     let contract = new web3.eth.Contract(tokenABI.ABI, tokenContractAddress);
     let pastEvents = await contract.getPastEvents("convertToken", {}, { fromBlock: fromBlock, toBlock: toBlock })
     updateLastProcessedBlock(toBlock)
